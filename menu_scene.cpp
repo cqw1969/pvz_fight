a0ce30dc3ccdb4b2ed4be216d1ea05bac9ed2054
chaosraider <chen13553730346@outlook.com>
2025-06-11 22:42:27 +0800

    issue

--- menu_scene.cpp
+++ menu_scene.cpp
@@ -1,1 +1,14 @@
 #include "menu_scene.h"
+void menuon_enter() {
+	std::cout << "进入主菜单" << std::endl;
+}
+void on_update() {
+	std::cout << "主菜单正在运行..." << std::endl;
+}
+void on_draw() {
+	outtextxy(10, 10, _T("主菜单绘图内容"));
+}
+void on_input(const ExMessage& msg) {}
+void on_exit() {
+	std::cout << "主菜单退出" << std::endl;
+}
--- menu_scene.h
+++ menu_scene.h
@@ -7,19 +7,11 @@ public:
 	MenuScene()=default;
 	~MenuScene()=default;
 	
-	void on_enter() {
-		std::cout << "进入主菜单" << std::endl;
-	}
-	void on_update() {
-		std::cout << "主菜单正在运行..." << std::endl;
-	}
-	void on_draw() {
-		outtextxy(10, 10, _T("主菜单绘图内容"));
-	}
-	void on_input(const ExMessage& msg){}
-	void on_exit() {
-		std::cout << "主菜单退出" << std::endl;
-	}
+	void on_enter();
+	void on_update();
+	void on_draw();
+	void on_input(const ExMessage& msg);
+	void on_exit();
 
 private:
 };
